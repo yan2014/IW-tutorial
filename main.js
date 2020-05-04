4965bdb94832feb524f2effcd2ec4c01de10fa83
Joan <s.jiang@umiami.edu>
2020-05-04 09:58:56 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function showresult1(){
+    var x = document.getElementById('x1').value;
+    var y = document.getElementById('y1').value;
+
+    console.log(x);
+
+    document.getElementById('result1').innerHTML=parseInt(x)+parseInt(y);
+    }
+
+
